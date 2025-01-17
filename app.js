9ba008c61fbee1f08fa75ac27487987a511998e5
Ana Claudia <a.claudia@estudante.ifro.edu.br>
2025-01-17 14:05:33 -0400

    Novos exercícios para exercitar a lógica de programação

--- app.js
+++ app.js
@@ -39,3 +39,43 @@ idadeUser = prompt('Digite sua idade:');
 if (idadeUser>= 18){
     alert('Pode tirar a habilitação!');
 }
+
+// 11 - Pergunte ao usuário qual é o dia da semana. Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!". Caso contrário, mostre "Boa semana!".
+
+dia = prompt('Qual o dia da semana?')
+
+if(dia == 'Sábado' || dia == 'Domingo'){
+    alert('Bom fim de semana!')
+}else{
+    alert('Boa semana!')
+}
+
+
+// 12 - Verifique se um número digitado pelo usuário é positivo ou negativo. Mostre um alerta informando.
+numero = prompt('Insira um número: ')
+
+if(numero > 0){
+    alert('Este número é positivo!')
+}else{
+    alert('Este número é negativo!')
+}
+
+// 13 - Crie um sistema de pontuação para um jogo. Se a pontuação for maior ou igual a 100, mostre "Parabéns, você venceu!". Caso contrário, mostre "Tente novamente para ganhar.".
+pontuacao = prompt('Pontuação: ')
+
+if(pontuacao >= 100){
+    alert('Parabéns, você venceu!')
+}else{
+    alert('Tente novamente para ganhar!')
+}
+
+// 14 - Crie uma mensagem que informa o usuário sobre o saldo da conta, usando uma template string para incluir o valor do saldo.
+saldo = 50
+
+alert('O saldo da conta é ' + saldo + ' reais.')
+
+// 15 - Peça ao usuário para inserir seu nome usando prompt. Em seguida, mostre um alerta de boas-vindas usando esse nome.
+nome = prompt('Insira seu nome: ')
+
+alert('Bem-vindo(a) ' + nome + '!')
+
