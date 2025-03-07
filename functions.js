cdf598cd749b9d7b118cf144c139b63e1d216406
Ana Claudia <a.claudia@estudante.ifro.edu.br>
2025-03-07 09:42:03 -0400

    Resolução de desafios sobre criações de funções em js

---
+++ functions.js
@@ -0,0 +1,74 @@
+//Desafios sobre funções
+
+/*1 - Criar uma função que exibe "Olá, mundo!" no console.
+function saudacao() {
+    console.log('Olá Mundo!');
+}
+
+saudacao();
+*/
+
+/*2 - Criar uma função que recebe um nome como parâmetro e exibe "Olá, [nome]!" no console.
+function saudacao(nome){
+    console.log('Olá, ' + nome + '!');
+    // console.log(`Olá, ${nome}!`);
+}
+
+saudacao('Ana');
+*/
+
+/* 3 - Criar uma função que recebe um número como parâmetro e retorna o dobro desse número.
+function calcularDobro(numero) {
+    return(numero * 2);
+}
+
+let resultado = calcularDobro(4);
+console.log(resultado);
+*/
+
+/* 4 - Criar uma função que recebe três números como parâmetros e retorna a média deles.
+function calcularMedia(numero1, numero2, numero3){
+    return((numero1 + numero2 + numero3) / 3);
+}
+
+let resultado = calcularMedia(2, 4, 6);
+console.log(resultado);
+*/
+
+/*
+5 - Criar uma função que recebe dois números como parâmetros e retorna o maior deles.
+function maiorNumero(numero1, numero2){
+    if(numero1 > numero2){
+        return numero1;
+    }
+    else if(numero1 == numero2){
+        return 'Esses números são iguais!';
+    }
+    else
+    {
+        return numero2;
+    }
+}
+
+//outra forma de fazer:
+//function encontrarMaior(a, b) {
+//    return a > b ? a : b;
+//}
+
+
+let resultado = maiorNumero(9,9);
+console.log(resultado);
+*/
+
+/* 6 - Criar uma função que recebe um número como parâmetro e retorna o resultado da multiplicação desse número por ele mesmo.
+function multiplicarNumero (numero){
+    return (numero * numero);
+}
+
+let resultado = multiplicarNumero(5);
+console.log(resultado);
+*/
+
+
+
+
